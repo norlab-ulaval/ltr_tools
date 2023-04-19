4d97f1aca52b58d2ca2506ccd076e3b5a272184a
Matěj Boxan <matej.boxan@gmail.cz>
2023-04-19 14:17:35 -0400

    Use .csv files in ltrEditTrajectory script

--- scripts/ltrEditTrajectory.sh
+++ scripts/ltrEditTrajectory.sh
@@ -6,7 +6,14 @@ if [ -z "$1" ]; then
 fi
 newFileName=$(printf '%s' "${1%.ltr}_edited.ltr")
 ltr2vtk_convertor "$1"
-echo "Starting ParaView, edit your trajectory and save it to /tmp/traj.vtk"
+editedTrajFileName="/tmp/traj.csv"
+echo "Starting ParaView, edit your trajectory and save it to $editedTrajFileName"
 paraview /usr/local/etc/${CMAKE_PROJECT_NAME}/paraview_config_edit.pvsm 2>&1 1>/dev/null
-vtk2ltr_convertor map.vtk traj.vtk "$newFileName"
+if [ -z "$editedTrajFileName" ]; then
+  echo "$editedTrajFileName missing, exiting"
+  exit
+fi
+# remove all quotes in the first column of the csv file and rename coordinates to xyz
+sed -i  -e 's/"//g' -e 's/Points:0/x/g' -e 's/Points:1/y/g' -e 's/Points:2/z/g' $editedTrajFileName
+vtk2ltr_convertor /tmp/map.vtk "$editedTrajFileName" "$newFileName"
 echo "Output saved to $newFileName"
